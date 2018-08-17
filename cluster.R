e9551bb7eac0e17dcd76b9f832610f6b96791fd7
kashyapmakadia <kashyapmakadia1997@gmail.com>
2018-08-17 17:19:57 +0530

    a

---
+++ cluster.R
@@ -0,0 +1,13 @@
+#clustering
+
+A=c(1,1.5,3.5,3.5,4.5,3.5)
+B=c(1,2,4,7,5.5,4.5)
+marks=data.frame(A,B)
+marks
+?kmeans
+(c1=kmeans(marks,3))
+cbind(marks,c1$cluster) #??
+plot(marks,pch=14,col=c1$cluster)
+c1$centers
+points(c1$centers,col=1:3,pch=20,cex=3)
+
